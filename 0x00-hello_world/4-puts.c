0c85ca85dd7301e7b7fa6a48bbb3c8f8be8dbe93
damareo1 <afolabi2cool@gmail.com>
2022-09-12 16:36:07 -0700

    ‘coding’

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+/**
+*main - print the string in the put function
+*
+* Description using the main function
+* this program print "programming is like building a puzzle
+*/
+int main(void)
+{
+        puts("\"programming is like multiligual puzzle");
+        return (0);
+}
